6c66e05ad746d74b4272aeb4f1d0cac38de7dd85
mireb53 <bgoodinc@gmail.com>
2026-01-18 17:10:13 +0800

    Add new FileViewer utilities export: only utility functions, no in-app viewers

---
+++ components/FileViewer/index.ts
@@ -0,0 +1,11 @@
+/**
+ * FileViewer Utilities
+ * 
+ * OLIN LMS no longer uses in-app file viewers.
+ * Files are downloaded to device storage and opened via Android intents.
+ * 
+ * This module only exports utility functions for file type detection and MIME types.
+ */
+
+export { detectFileType, getMimeType, formatFileSize } from './utils';
+export type { FileType } from './utils';
